0f3bea82c83894f6e8baa096aeea3c2b0404b5e4
Grigorjew Maxim <grigorjewmaxim@yandex.ru>
2023-05-15 20:23:38 +0500

    added function to save images

--- app/src/main/java/com/example/imageapplication/ChildActivity.java
+++ app/src/main/java/com/example/imageapplication/ChildActivity.java
@@ -2,15 +2,23 @@ package com.example.imageapplication;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.squareup.picasso.Picasso;
 
+import java.io.File;
 import java.io.FileOutputStream;
 
 
@@ -46,9 +54,36 @@ public class ChildActivity extends AppCompatActivity {
         load.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                saveToGallery();
             }
         });
+    }
+
+    private void saveToGallery() {
+        ActivityCompat.requestPermissions(ChildActivity.this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        ActivityCompat.requestPermissions(ChildActivity.this, new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        String folder = Environment.getExternalStorageDirectory() + "/Pictures/MyPics";
+        new File(folder).mkdir();
+        try {
+            BitmapDrawable bitmapDrawable = (BitmapDrawable) imageView.getDrawable();
+            Bitmap bitmap = bitmapDrawable.getBitmap();
+            File file = new File(folder, System.currentTimeMillis() / 1000 + ".jpg");
+            FileOutputStream outputStream = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 85, outputStream);
+            outputStream.flush();
+            outputStream.close();
+            MediaScannerConnection.scanFile(ChildActivity.this, new String[]{file.toString()}, null,
+                    new MediaScannerConnection.OnScanCompletedListener() {
+                        public void onScanCompleted(String path, Uri uri) {
+                            Log.i("ExternalStorage", "Scanned " + path + ":");
+                            Log.i("ExternalStorage", "-> uri=" + uri);
+                        }
+                    });
+            Toast.makeText(this, "Сохранено", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Toast.makeText(this, "Ошибка", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
     }
 }
 
--- app/src/main/java/com/example/imageapplication/Network.java
+++ app/src/main/java/com/example/imageapplication/Network.java
@@ -1,7 +1,6 @@
 package com.example.imageapplication;
 
 import android.net.Uri;
-import android.util.Log;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -15,6 +14,7 @@ public class Network {
     private static final String PARAM_API_KEY = "api_key";
     private static final String PARAM_TEXT = "text";
     private static final String PARAM_PAGE = "p";
+    private static final String API_KEY = "e55703c66330b8e5b24d1f4fee6fd30ac44ce6aa82643e6500ca26a27491b95a";
     private static String searches;
 
     public static URL generateURL(String search) {
@@ -22,7 +22,7 @@ public class Network {
         Uri biultUri = Uri.parse(BASE_URL)
                 .buildUpon()
                 .appendQueryParameter(PARAM_ENGINE, "yandex_images")
-                .appendQueryParameter(PARAM_API_KEY, "75761d4dbf6b14beadb2877122cf96c0b5ef8812f533ccd5c00fd3d3d195a9c4")
+                .appendQueryParameter(PARAM_API_KEY, API_KEY)
                 .appendQueryParameter(PARAM_PAGE, "0")
                 .appendQueryParameter(PARAM_TEXT, search)
                 .build();
@@ -40,7 +40,7 @@ public class Network {
         Uri biultUri = Uri.parse(BASE_URL)
                 .buildUpon()
                 .appendQueryParameter(PARAM_ENGINE, "yandex_images")
-                .appendQueryParameter(PARAM_API_KEY, "75761d4dbf6b14beadb2877122cf96c0b5ef8812f533ccd5c00fd3d3d195a9c4")
+                .appendQueryParameter(PARAM_API_KEY, API_KEY)
                 .appendQueryParameter(PARAM_PAGE, String.valueOf(count))
                 .appendQueryParameter(PARAM_TEXT, searches)
                 .build();
